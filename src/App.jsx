dac8eac06628c7d7fe4c8cbaf11a8332e2a6ba8c
Abhishk404 <bormanabhishekbarman123@gmail.com>
2025-12-15 21:49:29 +0530

    app.js page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ShopPage from "./pages/shopPage";
+import ShopPage from "./pages/shoppage";
 import HomePage from "./pages/Homepage";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import NotFound from "./pages/Notfount";
+import ScrollToTop from "./pages/Scrolltop";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
